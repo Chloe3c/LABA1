b2bd5c2e15e2d8fe08241b4c58f1b06bcd3cda70
chloe3c <chloe3c@209-1>
2024-09-27 17:51:06 +0200

    Добавьте файлы проекта.

---
+++ LABA1/Program.cs
@@ -0,0 +1,16 @@
+﻿//U = ln(k- y) + y**4 / e**y + 2,355 * k**2  записать арифметическое выражение на алг языке и указать порядок выполнения действий.
+try
+{
+    Console.Write("введите k: ");
+    int k = int.Parse(Console.ReadLine());
+    Console.Write("введите y: ");
+    int y = int.Parse(Console.ReadLine());
+    Console.Write("введите e: ");
+    int e = int.Parse(Console.ReadLine());
+    double U = (Math.Log(k - y) + Math.Pow(y, 4)) / (Math.Pow(e, y) + 2.355 * Math.Pow(k, 2));
+    Console.WriteLine($"{U:F2}");
+}
+catch 
+{
+    Console.Write("Введите правильно!");
+}
